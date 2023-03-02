f48a26cea3c393fdb32a878a982c49f1a01dbb28
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-03-02 16:36:31 -0500

    tiktok

--- Intro_to_Callbacks_Exercises.js
+++ Intro_to_Callbacks_Exercises.js
@@ -5,6 +5,20 @@ class Clock {
         this.minutes = date.getMinutes();
         this.seconds = date.getSeconds();
         
+        // fat arrow function
+        // setInterval(() => {
+        //   this._tick();
+        // }, 1000);
+        
+        // old version
+        // const that = this;
+        // setInterval(function() {
+        //   that._tick();
+        // }, 1000);
+
+        // bind version
+        setInterval(this._tick.bind(this), 1000);
+        
       // 1. Create a Date object.
       // 2. Store the hours, minutes, and seconds.
       // 3. Call printTime.
@@ -20,13 +34,21 @@ class Clock {
     _tick() {
       // 1. Increment the time by one second.
       // 2. Call printTime.
-      
-      setInterval(() => {
-        Date.setSeconds(this.seconds += 1);
-      }, 1000);
-      this.printTime();
+      this.seconds += 1
+      this.printTime()
+
+      if (this.seconds === 59) {
+        this.minutes += 1
+        this.seconds = -1
+        if (this.minutes >= 59) {
+          this.hours += 1
+          this.minutes = 0
+        }
+      //   // if (this.hours > 24) {
+
+        }
+      }
     }
-  }
   
   const clock = new Clock();
 
